4c964ac4ec0f6f9aafbf576702537340ac66c8f6
Tambua Evaristus <evaristustambua@gmail.com>
2022-06-07 16:01:08 +0100

    user info on drawer nav

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
-import * as React from 'react';
-import { Button, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
+import * as React from "react";
+import { Button, View, Text } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import DrawerContent from "./components/DrawerContent";
+// import {DrawerContent} from "./components/DrawerContent";
 
 function NotificationsScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Button onPress={() => navigation.goBack()} title="Go back home" />
     </View>
   );
@@ -27,8 +20,9 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+      <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Product" component={ProductScreen} />
         <Drawer.Screen name="Notifications" component={NotificationsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
---
+++ components/DrawerContent.js
@@ -0,0 +1,125 @@
+import { View, StyleSheet } from "react-native";
+
+import {
+  Title,
+  Text,
+  Avatar,
+  Caption,
+  Drawer,
+  TouchableRipple,
+  Switch,
+  Paragraph,
+} from "react-native-paper";
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
+import HomeScreen from "../screens/HomeScreen";
+import ProductScreen from "../screens/ProductScreen";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+function DrawerContent(props) {
+  //   const Drawer = createDrawerNavigator();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <DrawerContentScrollView {...props}>
+        <View style={styles.drawerContent}>
+          <View style={styles.userInfoSection}>
+            <View
+              style={{
+                flexDirection: "row",
+                marginTop: 15,
+              }}
+            >
+              <Avatar.Image source={require("../assets/mymy.jpg")} size={50} />
+              <View
+                style={{
+                  marginLeft: 15,
+                  flexDirection: "column",
+                }}
+              >
+                <Title>Tambua Evaristus</Title>
+                <Caption style={styles.Caption}>@evaristustambua</Caption>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.row}>
+            <View style={styles.section}>
+              <Paragraph style={[styles.paragraph, styles.caption]}>100</Paragraph>
+              <Caption style={styles.caption}>Followering</Caption>
+            </View>
+            <View style={styles.section}>
+              <Paragraph style={[styles.paragraph, styles.caption]}>60</Paragraph>
+              <Caption style={styles.caption}>Followers</Caption>
+            </View>
+          </View>
+        </View>
+      </DrawerContentScrollView>
+
+      <Drawer.Section style={styles.bottomDrawerSection}>
+        <DrawerItem
+          icon={({ color, size }) => (
+            <Icon name="exit-to-app" color={color} size={size} />
+          )}
+          label="Sign Out"
+          onPress={() => {}}
+        />
+      </Drawer.Section>
+    </View>
+  );
+}
+
+export default DrawerContent;
+
+const styles = StyleSheet.create({
+  drawerContent: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingLeft: 20,
+  },
+  title: {
+    fontSize: 16,
+    marginTop: 3,
+    fontWeight: "bold",
+  },
+  captions: {
+    fontSize: 14,
+    lineHeight: 14,
+  },
+  section: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 15,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 20,
+    justifyContent: "space-around"
+  },
+  paragraph: {
+    fontWeight: "bold",
+    marginRight: 3,
+  },
+  drawerSection: {
+    marginTop: 15,
+  },
+  bottomDrawerSection: {
+    marginBottom: 15,
+    borderBottomColor: "#f4f4f4",
+    borderTopWidth: 1,
+  },
+  preferences: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
+});
