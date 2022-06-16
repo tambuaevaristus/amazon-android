98b3fb1392ef17a4b121063aaa256ececd5e50ad
Tambua Evaristus <evaristustambua@gmail.com>
2022-06-16 16:50:11 +0100

    checkout total price

--- components/Checkout.js
+++ components/Checkout.js
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View} from "react-native";
+import React, {useState} from "react";
 import Product from "../components/Product";
 import { useStateValue } from "../StateProvider";
 import { VStack, HStack, Box, Divider, ScrollView } from "native-base";
 
 const Checkout = () => {
   const [{ basket }, dispatch] = useStateValue("");
+  const [price, setPrice] = useState(0);
+
+
+     const basketPrice = basket?.reduce((amount, item) => item.price + amount, 0);
+     setPrice(basketPrice);
+    
+     console.log(price)
 
   return (
     <VStack>
+      <View>
+        <Text>{price}</Text>
+      </View>
       {basket.map((item) => (
         <Product
           id={item.id}
